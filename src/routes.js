e4a775b107ba727bcfea5a788e6b141554a4b1f7
mranderson86 <mr.anderson.brito@gmail.com>
2020-03-24 21:40:21 -0300

    Criado rota de cadastro e listagem de ONGS

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,32 @@
 const express = require("express");
+const crypto = require("crypto");
+
+const connection = require("./database/connection");
 
 const routes = express.Router();
 
-routes.get("/", (request, response) => {
-  return response.send("Hello World");
+routes.get("/ongs", async (request, response) => {
+  const ongs = await connection("ongs").select("*");
+
+  return response.send(ongs);
+});
+
+// Cadatra uma nova ONG
+routes.post("/ongs", async (request, response) => {
+  const { name, email, whatsapp, city, uf } = request.body;
+
+  const id = crypto.randomBytes(4).toString("HEX");
+
+  await connection("ongs").insert({
+    id,
+    name,
+    email,
+    whatsapp,
+    city,
+    uf
+  });
+
+  return response.send({ id });
 });
 
 module.exports = routes;
